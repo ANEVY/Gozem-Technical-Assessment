e96fedb61c5ba360f98149ac97cb84b78d006975
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-10-28 12:32:52 +0100

    Create negative test suits

--- cypress/e2e/restful-booker-api.cy.js
+++ cypress/e2e/restful-booker-api.cy.js
@@ -1,5 +1,5 @@
 // import * as allure from "allure-js-commons";
-describe("Restful Booker API - CRUD Operations", () => {
+describe("Restful Booker API - CRUD Operations positive testing", () => {
   let bookingId; // To store the ID of the created booking
   let apiData;
   before(function () {
@@ -85,6 +85,113 @@ describe("Restful Booker API - CRUD Operations", () => {
       },
     }).then(function (response) {
       expect(response.status).to.eq(201);
+    });
+  });
+});
+
+describe("Restful Booker API - CRUD Operations negative testing", () => {
+  let bookingId; // To store the ID of the created booking
+  let apiData;
+  before(function () {
+    // Load test data from a fixture file
+    cy.fixture("apiData").then((data) => {
+      apiData = data;
+    });
+  });
+
+  //Create a new context for negative testing
+  it("Create a Booking with invalid data", function () {
+    cy.request({
+      method: "POST",
+      url: "/booking",
+      failOnStatusCode: false,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        firstname: apiData?.firstname || "John",
+        lastname: 100, // last name has to be string
+      },
+    }).then(function (response) {
+      // Verify the response
+      expect(response.status).to.eq(500);
+      expect(response.body).to.contain("Internal Server Error");
+    });
+  });
+  it("Get the Booking data for an invalid ID", function () {
+    cy.request({
+      method: "GET",
+      url: `/booking/${apiData.invalidBookingId}`, //invalid ID
+      failOnStatusCode: false,
+    }).then(function (response) {
+      cy.log(JSON.stringify(response.body));
+      expect(response.status).to.eq(404);
+      expect(response.body).to.contain("Not Found");
+    });
+  });
+  it("Update the booking (change checkout date and additional needs) without authenticating", function () {
+    cy.request({
+      method: "PUT",
+      url: `/booking/${bookingId}`,
+      failOnStatusCode: false,
+      body: {
+        firstname: apiData?.firstname || "John",
+        lastname: apiData?.lastname || "Cornors",
+        totalprice: apiData?.totalprice || 1000,
+        depositpaid: apiData?.depositpaid || true,
+        bookingdates: {
+          checkin: apiData?.checkin || "2024-10-25",
+          checkout: apiData?.updatedCheckout || "2024-10-30",
+        },
+        additionalneeds: apiData?.updatedAdditionalNeeds || "Dinner",
+      },
+    }).then(function (response) {
+      expect(response.status).to.eq(403);
+      expect(response.body).to.contain("Forbidden");
+    });
+  });
+  it("Update the booking (change checkout date and additional needs) with invalid checkout date", function () {
+    cy.request({
+      method: "PUT",
+      url: `/booking/${bookingId}`,
+      failOnStatusCode: false,
+      auth: {
+        username: Cypress.env("username"),
+        password: Cypress.env("password"),
+      },
+      body: {
+        bookingdates: {
+          checkout: 20241025,
+        },
+        additionalneeds: apiData?.updatedAdditionalNeeds || "Dinner",
+      },
+    }).then(function (response) {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.contain("Bad Request");
+    });
+  });
+  it("Delete booking without authenticating", function () {
+    cy.request({
+      method: "DELETE",
+      url: `/booking/${bookingId}`,
+      failOnStatusCode: false,
+    }).then(function (response) {
+      expect(response.status).to.eq(403);
+      expect(response.body).to.contain("Forbidden");
+    });
+  });
+  it("Delete booking for an invalid ID", function () {
+    cy.request({
+      method: "DELETE",
+      url: `/booking/${apiData.invalidBookingId}`,
+      failOnStatusCode: false,
+      auth: {
+        username: Cypress.env("username"),
+        password: Cypress.env("password"),
+      },
+    }).then(function (response) {
+      expect(response.status).to.eq(405);
+      expect(response.body).to.contain("Method Not Allowed");
     });
   });
 });
